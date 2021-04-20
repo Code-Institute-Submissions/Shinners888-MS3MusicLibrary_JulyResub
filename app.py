620d0c904080acd7c5ab462b0cdfd84ccc9bd5f1
Shinners888 <sinead888888888@gmail.com>
2021-04-20 19:38:38 +0000

    added functional registration page, linked to db, and styled

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 from flask import (
-    Flask, render_template,
+    Flask, flash, render_template,
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -24,6 +25,28 @@ mongo = PyMongo(app)
 def home():
     works = mongo.db.works.find()
     return render_template("home.html", works=works)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        user_already_exists = mongo.db.site_users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if user_already_exists:
+            flash("Oops, looks like someone already picked that username! Try again :)")
+            return redirect(url_for("register"))
+
+        register = {
+                "username": request.form.get("username").lower(),
+                "password": generate_password_hash(request.form.get("password"))
+            }
+        mongo.db.site_users.insert_one(register)
+
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful!")
+        return redirect(url_for("home", username=session["user"]))
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
